14a8ef7c2df09a13050b7cb542d5eba2e3851dc6
J Sinha <jsinha218@gmail.com>
2016-12-22 19:31:30 +0000

    Full_Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	##defining a custom helper for missing page titile
+	##full_title helper return "Ruby on rails Sample App" if 
+	##no page_title is there other wise returns |  Ruby on rails Sample App
+	def full_title(page_title = '')	
+		base_title = "Ruby on Rails Sample App"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " |"  + base_title
+		end
+	end				
+
 end
